f26633980a8a27bac0079f8da633a8f4464fd075
dok1959 <dok1959.good@mail.ru>
2021-08-08 00:05:27 +1000

    Добавлен чистый проект

---
+++ WebShell/WebShell/ClientApp/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <h2>Web shell page</h2>
+    );
+  }
+}
